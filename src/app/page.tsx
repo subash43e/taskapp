ac3d8028603d5ad7814edbb4bbd8ed8ecea4d6ed
Subash <131286689+subash43e@users.noreply.github.com>
2025-09-12 11:17:52 +0530

    first commit: Navigation bar created

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "../Components/NavBar";
+
+export default function Home() {
+
+  return (
+    <div>
+      {/* Navigation for Task Managing Application. */}
+      <section>
+        <nav>
+          <Navbar />
+        </nav>
+      </section>
+
+      <section>
+        {/* Body code here going */}
+        <h1>Body section</h1>
+      </section>
+
+    </div>
+  );
+}
