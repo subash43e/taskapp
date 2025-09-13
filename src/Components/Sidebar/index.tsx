cb6a97fa34d891e634ddb702612f8fc7fca61c47
Subash <131286689+subash43e@users.noreply.github.com>
2025-09-13 22:37:51 +0530

    Refactor Sidebar component and implement resizing functionality in Home

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -3,71 +3,74 @@ import React from 'react';
 export const Sidebar = () => {
 
   return (
-    <div className={`w-full bg-gray-800 text-white h-screen p-4`}>
+    <div className={`w-full bg-gray-800 text-white h-screen p-4 relative select-none`}>
       <h2 className="text-2xl font-bold mb-6">Tasks</h2>
-      <nav>
-        <ul>
-          <li className="mb-2">
-            <a href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">📥</span> Inbox
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">☀️</span> Today
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">📅</span> Calendar
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">🔜</span> Upcoming
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">✅</span> Completed
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              href="#"
-              className="flex items-center p-2 rounded-lg hover:bg-gray-700"
-              draggable={false}
-              onMouseDown={e => e.preventDefault()}
-            >
-              <span className="mr-3">🗑️</span> Trash
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className='text-white absolute right-0 top-5 cursor-pointer'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        </svg>
+      </div>
+      <ul>
+        <li className="mb-2">
+          <a href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">📥</span> Inbox
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">☀️</span> Today
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">📅</span> Calendar
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">🔜</span> Upcoming
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">✅</span> Completed
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="flex items-center p-2 rounded-lg hover:bg-gray-700"
+            draggable={false}
+            onMouseDown={e => e.preventDefault()}
+          >
+            <span className="mr-3">🗑️</span> Trash
+          </a>
+        </li>
+      </ul>
       <div className="mt-8">
         <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg flex items-center justify-center">
           <span className="mr-2">+</span> New Task
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,44 @@ import { Sidebar } from "../Components/Sidebar";
 
 export default function Home() {
 
-  const [size, setSize] = useState(350);
+  const [size, setSize] = useState(180);
   const [isResizing, setIsResizing] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [startWidth, setStartWidth] = useState(180);
 
-  const handleMouseDown = () => setIsResizing(true);
-
+  const handleMouseDown = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsResizing(true);
+    setStartX(e.clientX);
+    setStartWidth(size);
+  }
 
   useEffect(() => {
-
-    const handlingMouseMoving = (e) => {
+    const handlingMouseMoving = (e: MouseEvent) => {
       if (!isResizing) return;
       e.preventDefault();
-      const movement = e.movementX;
-      setSize(size + movement);
-      console.log(e);
+      
+      const currentX = e.clientX;
+      const diff = currentX - startX;
+      const newWidth = startWidth + diff;
+      
+      // Apply min and max constraints
+      const constrainedWidth = Math.min(Math.max(newWidth, 160), 300);
+      setSize(constrainedWidth);
     }
 
     const handleMouseUp = () => setIsResizing(false);
 
-    window.addEventListener("mousemove", handlingMouseMoving);
-    window.addEventListener("mouseup", handleMouseUp);
+    if (isResizing) {
+      window.addEventListener("mousemove", handlingMouseMoving);
+      window.addEventListener("mouseup", handleMouseUp);
+    }
 
     return () => {
       window.removeEventListener("mousemove", handlingMouseMoving);
       window.removeEventListener("mouseup", handleMouseUp);
     }
-  }, [size, isResizing])
+  }, [isResizing, startX, startWidth])
 
 
   return (
@@ -44,14 +56,15 @@ export default function Home() {
 
       <section className="bg-blue-900 h-screen flex" >
         {/* Side bar code going here. */}
-        <div style={{ width: `${size}px` }} >
+        <div style={{ width: `${size}px` }} className="min-w-[160px] max-w-[300px]" >
           <Sidebar />
         </div>
+
         <div
-          className={`w-1 top-0 bottom-0 right-0 cursor-col-resize hover:bg-blue-600 ${isResizing ? "bg-blue-600" : ""
-            }`}
-          onMouseDown={handleMouseDown}
+          className={`w-[2px] hover:w-1 top-0 bottom-0 right-0 cursor-col-resize hover:bg-blue-600 ${isResizing ? "bg-blue-600" : ""}`}
+          onMouseDown={handleMouseDown} 
         />
+
         {/* Body code here going here. */}
         <main className="resize-y border border-white bg-yellow-300 grow">
           <h1>hello</h1>
